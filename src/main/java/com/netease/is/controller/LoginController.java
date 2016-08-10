55afb0156ff0bb4940f29ad209720e36dbb312bf
刘一帝 <hzliuyidi@corp.netease.com>
2016-08-10 20:34:41 +0800

    新增登录控制

---
+++ src/main/java/com/netease/is/controller/LoginController.java
@@ -0,0 +1,47 @@
+package com.netease.is.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.web.util.WebUtils;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author hzliuyidi <liuyidi1993@gmail.com>
+ * @date 2016年8月10日
+ * @version 1.0
+ */
+@Controller
+public class LoginController {
+	
+	/**
+	 * 用户登录
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(HttpServletRequest request, HttpServletResponse response){
+		return "pages/login";
+	}
+	
+	/**
+	 * 用户登出
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public void logout(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		logoutService.logout(request, response);
+		WebUtils.issueRedirect(request, response, "/login");
+	}
+	
+	/**
+	 * 检测用户的登录权限,区分普通用户和管理员
+	 */
+}
--- src/main/java/com/netease/is/dao/UserDao.java
+++ src/main/java/com/netease/is/dao/UserDao.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserDao extends BaseDao<User>{
 	
+	/**
+	 * 根据邮箱号找到用户
+	 * @param email
+	 * @param pwd
+	 * @param type
+	 * @return
+	 */
 	public User findUser(String email,String pwd, Integer type){
 		return null;
 	}
